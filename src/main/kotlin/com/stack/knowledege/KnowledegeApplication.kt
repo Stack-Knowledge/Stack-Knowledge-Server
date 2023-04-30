c79b66d7c6a0a19bc745cd4a04d961ed21eac770
juhong <juhang720@gmail.com>
2023-05-01 08:02:27 +0900

    :tada: Begin a project

---
+++ src/main/kotlin/com/stack/knowledege/KnowledegeApplication.kt
@@ -0,0 +1,11 @@
+package com.stack.knowledege
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KnowledegeApplication
+
+fun main(args: Array<String>) {
+	runApplication<KnowledegeApplication>(*args)
+}
---
+++ src/test/kotlin/com/stack/knowledege/KnowledegeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.stack.knowledege
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KnowledegeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
