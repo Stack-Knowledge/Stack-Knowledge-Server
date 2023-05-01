48bf84cd16c865d846d2cef3a19d48f69c201224
juhong <juhang720@gmail.com>
2023-05-01 12:05:23 +0900

    :sparkles: 예외 핸들러 추가

---
+++ src/main/kotlin/com/stack/knowledege/global/error/exception/BasicException.kt
@@ -0,0 +1,5 @@
+package com.stack.knowledege.global.error.exception
+
+import com.stack.knowledege.global.error.ErrorCode
+
+open class BasicException(val errorCode: ErrorCode): RuntimeException(errorCode.message)
---
+++ src/main/kotlin/com/stack/knowledege/global/error/handler/GlobalExceptionHandler.kt
@@ -0,0 +1,20 @@
+package com.stack.knowledege.global.error.handler
+
+import com.stack.knowledege.global.error.ErrorResponse
+import com.stack.knowledege.global.error.exception.BasicException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(BasicException::class)
+    fun BasicExceptionHandler(e: BasicException): ResponseEntity<ErrorResponse> {
+        val errorCode = e.errorCode
+        return ResponseEntity(
+        ErrorResponse(message = errorCode.message, status = errorCode.status),
+        HttpStatus.valueOf(errorCode.status)
+        )
+    }
+}
