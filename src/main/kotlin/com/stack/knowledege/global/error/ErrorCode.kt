22e77e665735bcaf204a9b74efe587b546f2e632
juhong <juhang720@gmail.com>
2023-05-01 11:52:20 +0900

    :sparkles: error

---
+++ src/main/kotlin/com/stack/knowledege/global/error/ErrorCode.kt
@@ -0,0 +1,7 @@
+package com.stack.knowledege.global.error
+
+enum class ErrorCode(
+        val message: String,
+        val status: Int
+) {
+}
---
+++ src/main/kotlin/com/stack/knowledege/global/error/ErrorResponse.kt
@@ -0,0 +1,6 @@
+package com.stack.knowledege.global.error
+
+data class ErrorResponse(
+        val message: String,
+        val status: Int
+)
