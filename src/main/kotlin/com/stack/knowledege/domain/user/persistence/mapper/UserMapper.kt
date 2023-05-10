bbe2d45b9e3cf5a7bf66ac2d1a32b054d0c604a4
juhong <juhang720@gmail.com>
2023-05-10 11:48:32 +0900

    :sparkles: UserMapper

---
+++ src/main/kotlin/com/stack/knowledege/domain/user/persistence/mapper/UserMapper.kt
@@ -0,0 +1,33 @@
+package com.stack.knowledege.domain.user.persistence.mapper
+
+import com.stack.knowledege.domain.user.domain.User
+import com.stack.knowledege.domain.user.persistence.entity.UserEntity
+import com.stack.knowledege.global.mapper.GenericMapper
+import org.springframework.stereotype.Component
+
+@Component
+class UserMapper : GenericMapper<User, UserEntity> {
+    override fun toDomain(entity: UserEntity?): User? =
+        entity?.let {
+            User(
+                id = it.id,
+                email = it.email,
+                name = it.name,
+                grade = it.grade,
+                number = it.number,
+                roles = it.roles
+            )
+        }
+
+    override fun toEntity(domain: User): UserEntity =
+        domain?.let {
+            UserEntity(
+                id = it.id,
+                email = it.email,
+                name = it.name,
+                grade = it.grade,
+                number = it.number,
+                roles = it.roles
+            )
+        }
+}
