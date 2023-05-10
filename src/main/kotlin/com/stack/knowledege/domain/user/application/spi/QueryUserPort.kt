af973f3283c460baf636b8659f7cfc210390aa6f
juhong <juhang720@gmail.com>
2023-05-10 12:06:34 +0900

    :sparkles: User Port

---
+++ src/main/kotlin/com/stack/knowledege/domain/user/application/spi/QueryUserPort.kt
@@ -0,0 +1,11 @@
+package com.stack.knowledege.domain.user.application.spi
+
+import com.stack.knowledege.domain.user.domain.User
+import java.util.UUID
+
+interface QueryUserPort {
+    fun queryCurrentUser(): User
+
+    fun queryUserById(id: UUID): User?
+
+}
