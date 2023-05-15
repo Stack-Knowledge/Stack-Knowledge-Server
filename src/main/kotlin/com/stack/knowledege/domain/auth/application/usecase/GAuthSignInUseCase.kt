26c32e60568c599bbf1e182846490306a245b524
park juhong <parkjuhong@juhongs-MacBook-Pro.local>
2023-05-15 21:10:01 +0900

    :sparkles: Gauth SignIn Use

---
+++ src/main/kotlin/com/stack/knowledege/domain/auth/application/usecase/GAuthSignInUseCase.kt
@@ -0,0 +1,43 @@
+package com.stack.knowledege.domain.auth.application.usecase
+
+import com.stack.knowledege.domain.auth.presentation.data.response.TokenResponse
+import com.stack.knowledege.domain.user.application.spi.UserPort
+import com.stack.knowledege.domain.user.domain.User
+import com.stack.knowledege.domain.user.exception.UserNotFoundException
+import com.stack.knowledege.global.security.spi.JwtGeneratorPort
+import com.stack.knowledege.thirdparty.gauth.spi.GAuthPort
+import java.util.*
+
+class GAuthSignInUseCase(
+    private val gAuthPort: GAuthPort,
+    private val userPort: UserPort,
+    private val jwtGeneratorPort: JwtGeneratorPort
+) {
+    fun execute(code: String): TokenResponse {
+        val gauthToken = gAuthPort.queryGAuthToken(code)
+        val gauthUserInfo = gAuthPort.queryUserInfo(gauthToken.accessToken)
+        val role = userPort.queryUserRoleByEmail(gauthUserInfo.email)
+
+        val user = createUser(
+            userPort.queryExistByEmail(gauthUserInfo.email),
+            User(
+                id = UUID.randomUUID(),
+                email = gauthUserInfo.email,
+                name = gauthUserInfo.name,
+                grade = gauthUserInfo.grade,
+                number = gauthUserInfo.num,
+                roles = mutableListOf(role)
+            )
+        )
+
+        return jwtGeneratorPort.receiveToken(user.email)
+    }
+
+    private fun createUser(isExistUser: Boolean, user: User): User {
+        return if (isExistUser) {
+            userPort.queryUserByEmail(user.email) ?: throw UserNotFoundException()
+        } else {
+            userPort.saveUser(user)!!
+        }
+    }
+}
--- src/main/kotlin/com/stack/knowledege/domain/user/application/spi/QueryUserPort.kt
+++ src/main/kotlin/com/stack/knowledege/domain/user/application/spi/QueryUserPort.kt
@@ -1,10 +1,13 @@
 package com.stack.knowledege.domain.user.application.spi
 
 import com.stack.knowledege.domain.user.domain.User
+import com.stack.knowledege.domain.user.domain.constant.UserRole
 import java.util.UUID
 
 interface QueryUserPort {
     fun queryCurrentUser(): User
-
     fun queryUserById(id: UUID): User?
+    fun queryUserRoleByEmail(email: String): UserRole
+    fun queryUserByEmail(email: String): User?
+    fun queryExistByEmail(email: String): Boolean
 }
