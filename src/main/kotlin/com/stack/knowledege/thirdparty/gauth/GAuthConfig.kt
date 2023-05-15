3bd0145c627a0f1cee55efb0e76a3586cc3c3867
park juhong <parkjuhong@juhongs-MacBook-Pro.local>
2023-05-15 17:46:03 +0900

    :wrench: GAuth Configuration

---
+++ src/main/kotlin/com/stack/knowledege/thirdparty/gauth/GAuthConfig.kt
@@ -0,0 +1,12 @@
+package com.stack.knowledege.thirdparty.gauth
+
+import gauth.GAuth
+import gauth.impl.GAuthImpl
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class GAuthConfig {
+    @Bean
+    fun gauth(): GAuth = GAuthImpl()
+}
