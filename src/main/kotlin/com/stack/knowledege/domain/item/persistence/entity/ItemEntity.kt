aed2720de2fa47708faa3fd2eb407e33e5948e10
JuuuuHong <juhang720@gmail.com>
2023-05-17 12:24:18 +0900

    :sparkles: Item entity

---
+++ src/main/kotlin/com/stack/knowledege/domain/item/persistence/entity/ItemEntity.kt
@@ -0,0 +1,15 @@
+package com.stack.knowledege.domain.item.persistence.entity
+
+import com.stack.knowledege.global.entity.BaseUuidEntity
+import java.util.*
+import javax.persistence.Column
+
+class ItemEntity(
+    override val id: UUID,
+    @Column(nullable = false)
+    val name: String,
+    @Column(nullable = false)
+    val text: String,
+    @Column(nullable = false)
+    val price: Int
+) : BaseUuidEntity(id)
