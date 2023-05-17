2349214f0276cda4bb4213199beebfea8b7fdca5
JuuuuHong <juhang720@gmail.com>
2023-05-18 00:12:23 +0900

    :sparkles: Item persistence adapter

--- src/main/kotlin/com/stack/knowledege/domain/item/application/spi/QueryItemPort.kt
+++ src/main/kotlin/com/stack/knowledege/domain/item/application/spi/QueryItemPort.kt
@@ -4,6 +4,6 @@ import com.stack.knowledege.domain.item.domain.Item
 import java.util.UUID
 
 interface QueryItemPort {
-    fun queryAllItem(): List<Item>
-    fun queryItemById(itemId: UUID): Item
+    fun queryAllItem(): List<Item?>
+    fun queryItemById(itemId: UUID): Item?
 }
---
+++ src/main/kotlin/com/stack/knowledege/domain/item/persistence/ItemPersistenceAdapter.kt
@@ -0,0 +1,26 @@
+package com.stack.knowledege.domain.item.persistence
+
+import com.stack.knowledege.domain.item.application.spi.ItemPort
+import com.stack.knowledege.domain.item.domain.Item
+import com.stack.knowledege.domain.item.persistence.mapper.ItemMapper
+import com.stack.knowledege.domain.item.persistence.repository.ItemRepository
+import org.springframework.data.repository.findByIdOrNull
+import java.util.*
+
+class ItemPersistenceAdapter(
+    private val itemRepository: ItemRepository,
+    private val itemMapper: ItemMapper
+) : ItemPort {
+    override fun saveItem(item: Item): Item =
+        itemMapper.toDomain(itemRepository.save(itemMapper.toEntity(item)))!!
+
+
+    override fun deleteItem(itemId: UUID) =
+        itemRepository.deleteById(itemId)
+
+    override fun queryAllItem(): List<Item?> =
+        itemRepository.findAll().map { itemMapper.toDomain(it) }
+
+    override fun queryItemById(itemId: UUID): Item? =
+        itemMapper.toDomain(itemRepository.findByIdOrNull(itemId))
+}
