f449703a221eed4a77f189b20e3b0f856efa2f24
JuuuuHong <juhang720@gmail.com>
2023-05-17 19:40:27 +0900

    :recycle: User port, adapter 수정

--- src/main/kotlin/com/stack/knowledege/domain/user/persistence/UserPersistenceAdapter.kt
+++ src/main/kotlin/com/stack/knowledege/domain/user/persistence/UserPersistenceAdapter.kt
@@ -2,6 +2,7 @@ package com.stack.knowledege.domain.user.persistence
 
 import com.stack.knowledege.domain.user.application.spi.UserPort
 import com.stack.knowledege.domain.user.domain.User
+import com.stack.knowledege.domain.user.domain.constant.UserRole
 import com.stack.knowledege.domain.user.exception.UserNotFoundException
 import com.stack.knowledege.domain.user.persistence.mapper.UserMapper
 import com.stack.knowledege.domain.user.persistence.repository.UserRepository
@@ -19,10 +20,31 @@ class UserPersistenceAdapter(
     override fun saveUser(user: User): User? =
         userMapper.toDomain(userRepository.save(userMapper.toEntity(user)))
 
-    override fun queryCurrentUser(): User =
-        userMapper.toDomain(userRepository.findByIdOrNull(securityPort.queryCurrentUserId()))
-            .let { it ?: throw UserNotFoundException() }
+    override fun queryUserById(id: UUID): User? {
+            val user = userRepository.findByIdOrNull(id)
+            return userMapper.toDomain(user)
+    }
 
-    override fun queryUserById(id: UUID): User? =
-        userMapper.toDomain(userRepository.findByIdOrNull(id) ?: throw UserNotFoundException())
+
+//    override fun queryUserRoleByEmail(email: String): UserRole {
+//        val user = userRepository.findByEmail(email)
+//        return when () {
+//
+//        }
+//    }
+
+    override fun queryUserByEmail(email: String): User? {
+        TODO("Not yet implemented")
+    }
+
+    override fun queryExistByEmail(email: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun queryCurrentUser(): User {
+        return userMapper.toDomain(userRepository.findByIdOrNull(securityPort.queryCurrentUserId()))
+            .let {
+                it ?: throw UserNotFoundException()
+            }
+    }
 }
