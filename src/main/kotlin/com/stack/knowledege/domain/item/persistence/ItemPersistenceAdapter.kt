0cc5be1fc27f94eb5cb81cca7463c73a0bbad89a
JuuuuHong <juhang720@gmail.com>
2023-05-18 08:06:52 +0900

    :sparkles: Query all item usecase

--- src/main/kotlin/com/stack/knowledege/domain/item/persistence/ItemPersistenceAdapter.kt
+++ src/main/kotlin/com/stack/knowledege/domain/item/persistence/ItemPersistenceAdapter.kt
@@ -5,8 +5,10 @@ import com.stack.knowledege.domain.item.domain.Item
 import com.stack.knowledege.domain.item.persistence.mapper.ItemMapper
 import com.stack.knowledege.domain.item.persistence.repository.ItemRepository
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
 import java.util.*
 
+@Component
 class ItemPersistenceAdapter(
     private val itemRepository: ItemRepository,
     private val itemMapper: ItemMapper
---
+++ src/main/kotlin/com/stack/knowledege/domain/item/presentation/data/response/ItemResponse.kt
@@ -0,0 +1,10 @@
+package com.stack.knowledege.domain.item.presentation.data.response
+
+import java.util.*
+
+data class ItemResponse(
+    val id: UUID,
+    val name: String,
+    val text: String,
+    val price: Int
+)
