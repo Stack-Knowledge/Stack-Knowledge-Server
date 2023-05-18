4d615b7468734a43c0a5754bb142b7079a6d6daa
JuuuuHong <juhang720@gmail.com>
2023-05-18 14:50:22 +0900

    :sparkles: QueryItembyid api

--- src/main/kotlin/com/stack/knowledege/domain/item/presentation/ItemWebAdapter.kt
+++ src/main/kotlin/com/stack/knowledege/domain/item/presentation/ItemWebAdapter.kt
@@ -1,19 +1,28 @@
 package com.stack.knowledege.domain.item.presentation
 
 import com.stack.knowledege.domain.item.application.usecase.QueryAllItemUseCase
+import com.stack.knowledege.domain.item.application.usecase.QueryItemByIdUseCase
 import com.stack.knowledege.domain.item.presentation.data.response.ItemResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/item")
 class ItemWebAdapter(
-    private val queryAllItemUseCase: QueryAllItemUseCase
+    private val queryAllItemUseCase: QueryAllItemUseCase,
+    private val queryItemByIdUseCase: QueryItemByIdUseCase
 ) {
     @GetMapping
     fun queryAllItem(): ResponseEntity<List<ItemResponse>> =
         queryAllItemUseCase.execute()
+            .let { ResponseEntity.ok(it) }
+
+    @GetMapping("/{item-id}")
+    fun queryItemById(@PathVariable itemId: UUID): ResponseEntity<ItemResponse> =
+        queryItemByIdUseCase.execute(itemId)
             .let { ResponseEntity.ok(it) }
 }
