f86e2b927c016cbd30b10378456dbbe2798a4fc8
JuuuuHong <juhang720@gmail.com>
2023-05-31 08:21:26 +0900

    student persistence adapter

---
+++ src/main/kotlin/com/stack/knowledege/domain/student/persistence/StudentPersistenceAdapter.kt
@@ -0,0 +1,22 @@
+package com.stack.knowledege.domain.student.persistence
+
+import com.stack.knowledege.domain.student.application.spi.StudentPort
+import com.stack.knowledege.domain.student.domain.Student
+import com.stack.knowledege.domain.student.persistence.mapper.StudentMapper
+import com.stack.knowledege.domain.student.persistence.repository.StudentJpaRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class StudentPersistenceAdapter(
+    private val studentJpaRepository: StudentJpaRepository,
+    private val studentMapper: StudentMapper
+) : StudentPort{
+    override fun save(student: Student) {
+        studentJpaRepository.save(studentMapper.toEntity(student))
+    }
+
+    override fun queryStudentById(id: UUID): Student? =
+        studentMapper.toDomain(studentJpaRepository.findByIdOrNull(id))
+}
