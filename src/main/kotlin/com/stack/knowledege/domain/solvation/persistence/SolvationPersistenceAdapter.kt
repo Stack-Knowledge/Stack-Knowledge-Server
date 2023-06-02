45d6ca54e9eada7d088590163cdd8c90d9943321
JuuuuHong <juhang720@gmail.com>
2023-06-02 21:49:53 +0900

    :sparkles: Solvation persistence adapter

---
+++ src/main/kotlin/com/stack/knowledege/domain/solvation/persistence/SolvationPersistenceAdapter.kt
@@ -0,0 +1,23 @@
+package com.stack.knowledege.domain.solvation.persistence
+
+import com.stack.knowledege.domain.solvation.application.spi.SolvationPort
+import com.stack.knowledege.domain.solvation.domain.Solvation
+import com.stack.knowledege.domain.solvation.persistence.mapper.SolvationMapper
+import com.stack.knowledege.domain.solvation.persistence.repository.SolvationRepository
+import org.springframework.data.repository.findByIdOrNull
+import java.util.*
+
+class SolvationPersistenceAdapter(
+    private val solvationRepository: SolvationRepository,
+    private val solvationMapper: SolvationMapper
+) : SolvationPort {
+    override fun save(solvation: Solvation) {
+        solvationRepository.save(solvationMapper.toEntity(solvation))
+    }
+
+    override fun queryAllSolvation(): List<Solvation> =
+        solvationRepository.findAll().map { solvationMapper.toDomain(it)!! }
+
+    override fun querySolvationById(solvationId: UUID): Solvation? =
+        solvationMapper.toDomain(solvationRepository.findByIdOrNull(solvationId))
+}
