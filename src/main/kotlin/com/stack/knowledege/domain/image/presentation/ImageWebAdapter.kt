6706e7f1f3cfed002bea9d58a1a888bfda45e3cf
JuuuuHong <juhang720@gmail.com>
2023-06-08 23:55:32 +0900

    :sparkles: 메서드 이름이 왜이래

--- src/main/kotlin/com/stack/knowledege/domain/image/presentation/ImageWebAdapter.kt
+++ src/main/kotlin/com/stack/knowledege/domain/image/presentation/ImageWebAdapter.kt
@@ -2,6 +2,7 @@ package com.stack.knowledege.domain.image.presentation
 
 import com.stack.knowledege.domain.image.application.usecase.UploadImageUseCase
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
@@ -11,10 +12,16 @@ import org.springframework.web.multipart.MultipartFile
 @RestController
 @RequestMapping("/image")
 class ImageWebAdapter(
-    private val updateImageUseCase: UploadImageUseCase
+    private val updateImageUseCase: UploadImageUseCase,
+    private val uploadImageUseCase: UploadImageUseCase
 ) {
     @PostMapping
-    fun execute(@RequestPart multipartFile: MultipartFile): ResponseEntity<String> =
+    fun uploadImage(@RequestPart multipartFile: MultipartFile): ResponseEntity<String> =
         updateImageUseCase.execute(multipartFile)
+            .let { ResponseEntity.ok(it) }
+
+    @PatchMapping
+    fun updateImage(multipartFile: MultipartFile): ResponseEntity<String> =
+        uploadImageUseCase.execute(multipartFile)
             .let { ResponseEntity.ok(it) }
 }
