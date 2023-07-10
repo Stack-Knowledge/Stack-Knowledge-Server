35bf909301ef671422d221926fda0bcb1ef239a7
JuuuuHong <juhang720@gmail.com>
2023-07-10 13:50:12 +0900

    :recycle: 사용하지 않는 클래스 삭제

--- src/main/kotlin/com/stack/knowledege/domain/mission/application/usecase/QueryMissionDetailsUseCase.kt
+++ src/main/kotlin/com/stack/knowledege/domain/mission/application/usecase/QueryMissionDetailsUseCase.kt
@@ -1,7 +1,6 @@
 package com.stack.knowledege.domain.mission.application.usecase
 
 import com.stack.knowledege.domain.mission.application.spi.MissionPort
-import com.stack.knowledege.domain.mission.exception.AlreadySolvedMissionException
 import com.stack.knowledege.domain.mission.exception.MissionNotFoundException
 import com.stack.knowledege.domain.mission.presentation.data.response.MissionDetailsResponse
 import com.stack.knowledege.domain.user.application.spi.QueryUserPort
