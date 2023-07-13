b1ed33e9baf95ed43aefdbab2e8e732f8a4065a4
JuuuuHong <juhang720@gmail.com>
2023-07-13 16:16:39 +0900

    :recycle: Title과 introduce 합병

--- src/main/kotlin/com/stack/knowledege/domain/mission/presentation/data/request/CreateMissionRequest.kt
+++ src/main/kotlin/com/stack/knowledege/domain/mission/presentation/data/request/CreateMissionRequest.kt
@@ -2,7 +2,6 @@ package com.stack.knowledege.domain.mission.presentation.data.request
 
 data class CreateMissionRequest(
     val title: String,
-    val introduce: String,
     val content: String,
     val timeLimit: Int
 )
