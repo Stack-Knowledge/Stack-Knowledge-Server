f16a43b2a5f27a7504ab9c9ade7b33bcc7ab8f04
JuuuuHong <juhang720@gmail.com>
2023-07-17 00:11:36 +0900

    :sparkles: Timezone 한국으로 수정

--- src/main/kotlin/com/stack/knowledege/KnowledgeApplication.kt
+++ src/main/kotlin/com/stack/knowledege/KnowledgeApplication.kt
@@ -3,11 +3,13 @@ package com.stack.knowledege
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class KnowledgeApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
 	runApplication<KnowledgeApplication>(*args)
 }
