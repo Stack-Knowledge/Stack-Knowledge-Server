ebb6c5727962c20d7d520c2e5ada18246e774a36
JuuuuHong <juhang720@gmail.com>
2023-07-28 01:24:47 +0900

    :sparkles: Query all is_ordered item usecase

--- src/main/kotlin/com/stack/knowledege/domain/order/application/spi/QueryOrderPort.kt
+++ src/main/kotlin/com/stack/knowledege/domain/order/application/spi/QueryOrderPort.kt
@@ -1,7 +1,9 @@
 package com.stack.knowledege.domain.order.application.spi
 
 import com.stack.knowledege.domain.order.domain.Order
+import com.stack.knowledege.domain.order.domain.constant.OrderStatus
 
 interface QueryOrderPort {
     fun queryAllOder(): List<Order>
+    fun queryAllIsOrderedItem(orderStatus: OrderStatus): List<Order>
 }
--- src/main/kotlin/com/stack/knowledege/domain/order/persistence/OrderPersistenceAdapter.kt
+++ src/main/kotlin/com/stack/knowledege/domain/order/persistence/OrderPersistenceAdapter.kt
@@ -2,6 +2,7 @@ package com.stack.knowledege.domain.order.persistence
 
 import com.stack.knowledege.domain.order.application.spi.OrderPort
 import com.stack.knowledege.domain.order.domain.Order
+import com.stack.knowledege.domain.order.domain.constant.OrderStatus
 import com.stack.knowledege.domain.order.persistence.mapper.OrderMapper
 import com.stack.knowledege.domain.order.persistence.repository.OrderRepository
 import org.springframework.stereotype.Component
@@ -17,4 +18,7 @@ class OrderPersistenceAdapter(
 
     override fun queryAllOder(): List<Order> =
         orderRepository.findAll().map { orderMapper.toDomain(it)!! }
+
+    override fun queryAllIsOrderedItem(orderStatus: OrderStatus): List<Order> =
+        orderRepository.findByOrderStatus(orderStatus).map { orderMapper.toDomain(it)!! }
 }
