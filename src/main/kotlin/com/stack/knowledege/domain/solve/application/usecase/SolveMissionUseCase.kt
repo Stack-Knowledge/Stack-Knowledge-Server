c787d2b6b25a5a1735bc3a0c88676b748b9ec364
JuuuuHong <juhang720@gmail.com>
2023-07-28 13:40:59 +0900

    :ambulance: Conflict 해결

--- src/main/kotlin/com/stack/knowledege/domain/solve/application/usecase/SolveMissionUseCase.kt
+++ src/main/kotlin/com/stack/knowledege/domain/solve/application/usecase/SolveMissionUseCase.kt
@@ -10,6 +10,7 @@ import com.stack.knowledege.domain.solve.domain.Solve
 import com.stack.knowledege.domain.solve.domain.constant.SolveStatus
 import com.stack.knowledege.domain.solve.exception.StudentOnlyException
 import com.stack.knowledege.domain.student.application.spi.QueryStudentPort
+import com.stack.knowledege.domain.student.exception.StudentNotFoundException
 import com.stack.knowledege.domain.user.application.spi.QueryUserPort
 import com.stack.knowledege.domain.user.domain.constant.UserRole
 import com.stack.knowledege.global.annotation.usecase.UseCase
@@ -32,7 +33,7 @@ class SolveMissionUseCase(
         if (user.roles.firstOrNull() != UserRole.ROLE_STUDENT)
             throw StudentOnlyException()
 
-        val student = queryStudentPort.queryStudentByUser(user)
+        val student = queryStudentPort.queryStudentByUser(user) ?: throw StudentNotFoundException()
 
         val solve = Solve(
             id = UUID.randomUUID(),
