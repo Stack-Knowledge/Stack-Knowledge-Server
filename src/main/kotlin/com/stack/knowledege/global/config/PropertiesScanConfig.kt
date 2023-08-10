3d0b0cd656b84d16c2fdf24a4f822800bdfc6449
JuuuuHong <juhang720@gmail.com>
2023-08-10 19:22:27 +0900

    update :: aws properties 빠져있는 거 추가

--- src/main/kotlin/com/stack/knowledege/global/config/PropertiesScanConfig.kt
+++ src/main/kotlin/com/stack/knowledege/global/config/PropertiesScanConfig.kt
@@ -1,6 +1,7 @@
 package com.stack.knowledege.global.config
 
 import com.stack.knowledege.global.security.token.properties.JwtProperties
+import com.stack.knowledege.thirdparty.aws.AwsProperties
 import com.stack.knowledege.thirdparty.gauth.GAuthProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
@@ -9,7 +10,8 @@ import org.springframework.context.annotation.Configuration
 @ConfigurationPropertiesScan(
     basePackageClasses = [
         JwtProperties::class,
-        GAuthProperties::class
+        GAuthProperties::class,
+        AwsProperties::class
     ]
 )
 class PropertiesScanConfig
