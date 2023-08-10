3242c6cd6f5d5cad4732de6b95d668e5db604e91
JuuuuHong <juhang720@gmail.com>
2023-08-10 18:27:46 +0900

    refactor :: refreshToken Mapper

--- src/main/kotlin/com/stack/knowledege/domain/auth/persistence/mapper/RefreshTokenMapper.kt
+++ src/main/kotlin/com/stack/knowledege/domain/auth/persistence/mapper/RefreshTokenMapper.kt
@@ -2,26 +2,30 @@ package com.stack.knowledege.domain.auth.persistence.mapper
 
 import com.stack.knowledege.domain.auth.domain.RefreshToken
 import com.stack.knowledege.domain.auth.persistence.entity.RefreshTokenEntity
-import com.stack.knowledege.global.mapper.GenericMapper
-import org.springframework.stereotype.Component
+import org.mapstruct.InjectionStrategy
+import org.mapstruct.Mapper
+import org.mapstruct.Mapping
+import org.mapstruct.MappingConstants
+import org.mapstruct.Mappings
+import org.mapstruct.ReportingPolicy
 
-@Component
-class RefreshTokenMapper() : GenericMapper<RefreshToken, RefreshTokenEntity> {
-    override fun toDomain(entity: RefreshTokenEntity?): RefreshToken? =
-        entity?.let {
-            RefreshToken(
-                refreshToken = it.refreshToken,
-                userId = it.userId,
-                expiredAt = it.expiredAt
-            )
-        }
+@Mapper(
+    componentModel = MappingConstants.ComponentModel.SPRING,
+    injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+    unmappedTargetPolicy = ReportingPolicy.WARN
+)
+interface RefreshTokenMapper {
+    @Mappings(
+        Mapping(source = "refreshToken", target = "refreshToken"),
+        Mapping(source = "userId", target = "userId"),
+        Mapping(source = "expiredAt", target = "expiredAt")
+    )
+    fun toDomain(refreshTokenEntity: RefreshTokenEntity?): RefreshToken?
 
-    override fun toEntity(domain: RefreshToken): RefreshTokenEntity =
-        domain?.let {
-            RefreshTokenEntity(
-                refreshToken = it.refreshToken,
-                userId = it.userId,
-                expiredAt = it.expiredAt
-            )
-        }
+    @Mappings(
+        Mapping(source = "refreshToken", target = "refreshToken"),
+        Mapping(source = "userId", target = "userId"),
+        Mapping(source = "expiredAt", target = "expiredAt")
+    )
+    fun toEntity(refreshToken: RefreshToken): RefreshTokenEntity
 }
