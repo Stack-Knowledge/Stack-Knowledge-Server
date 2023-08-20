54161d97c83a49a3a0696da1570d5d4143ab0f2e
JuuuuHong <juhang720@gmail.com>
2023-08-21 03:33:30 +0900

    refactor :: solve mission 완성

--- src/main/kotlin/com/stack/knowledege/domain/point/persistence/PointPersistenceAdapter.kt
+++ src/main/kotlin/com/stack/knowledege/domain/point/persistence/PointPersistenceAdapter.kt
@@ -1,6 +1,7 @@
 package com.stack.knowledege.domain.point.persistence
 
 import com.stack.knowledege.domain.mission.domain.Mission
+import com.stack.knowledege.domain.mission.persistence.mapper.MissionMapper
 import com.stack.knowledege.domain.point.application.spi.PointPort
 import com.stack.knowledege.domain.point.domain.Point
 import com.stack.knowledege.domain.point.persistence.mapper.PointMapper
@@ -10,15 +11,16 @@ import org.springframework.stereotype.Component
 @Component
 class PointPersistenceAdapter(
     private val pointJpaRepository: PointJpaRepository,
-    private val pointMapper: PointMapper
+    private val pointMapper: PointMapper,
+    private val missionMapper: MissionMapper
 ) : PointPort {
     override fun save(point: Point) {
         pointJpaRepository.save(pointMapper.toEntity(point))
     }
 
     override fun queryPointByMission(mission: Mission): Point? =
-        pointMapper.toDomain(pointJpaRepository.findByMission(mission))
+        pointMapper.toDomain(pointJpaRepository.findByMission(missionMapper.toEntity(mission)))
 
     override fun queryPointTopByIdDesc(): Point? =
-        pointMapper.toDomain(pointJpaRepository.findTopByOrderByIdDesc())
+        pointMapper.toDomain(pointJpaRepository.findTopByOrderByMissionPointDesc())
 }
--- src/main/kotlin/com/stack/knowledege/domain/point/persistence/repository/PointJpaRepository.kt
+++ src/main/kotlin/com/stack/knowledege/domain/point/persistence/repository/PointJpaRepository.kt
@@ -1,10 +1,10 @@
 package com.stack.knowledege.domain.point.persistence.repository
 
-import com.stack.knowledege.domain.mission.domain.Mission
+import com.stack.knowledege.domain.mission.persistence.entity.MissionJpaEntity
 import com.stack.knowledege.domain.point.persistence.entity.PointJpaEntity
 import org.springframework.data.repository.CrudRepository
 
 interface PointJpaRepository : CrudRepository<PointJpaEntity, Long> {
-    fun findByMission(mission: Mission): PointJpaEntity
-    fun findTopByOrderByIdDesc(): PointJpaEntity
+    fun findByMission(missionJpaEntity: MissionJpaEntity): PointJpaEntity
+    fun findTopByOrderByMissionPointDesc(): PointJpaEntity?
 }
