c7976a9ecf2e5b2ed829d41df5b01e86aa4c6b5d
JuuuuHong <juhang720@gmail.com>
2023-08-23 00:25:59 +0900

    refactor :: dto 파라미터 변경

--- src/main/kotlin/com/stack/knowledege/domain/order/application/usecase/OrderItemUseCase.kt
+++ src/main/kotlin/com/stack/knowledege/domain/order/application/usecase/OrderItemUseCase.kt
@@ -1,46 +1,47 @@
 package com.stack.knowledege.domain.order.application.usecase
 
 import com.stack.knowledege.domain.item.application.spi.QueryItemPort
-import com.stack.knowledege.domain.item.exception.ItemNotFoundException
 import com.stack.knowledege.domain.order.application.spi.CommandOrderPort
 import com.stack.knowledege.domain.order.domain.Order
 import com.stack.knowledege.domain.order.domain.constant.OrderStatus
 import com.stack.knowledege.domain.order.exception.LackPointException
 import com.stack.knowledege.domain.order.presentation.data.request.OrderItemRequest
-import com.stack.knowledege.domain.student.application.spi.QueryStudentPort
 import com.stack.knowledege.domain.student.exception.StudentNotFoundException
 import com.stack.knowledege.common.annotation.usecase.UseCase
 import com.stack.knowledege.common.service.SecurityService
+import com.stack.knowledege.domain.student.application.spi.StudentPort
 import java.util.UUID
 
 @UseCase
 class OrderItemUseCase(
     private val queryItemPort: QueryItemPort,
     private val securityService: SecurityService,
-    private val queryStudentPort: QueryStudentPort,
+    private val studentPort: StudentPort,
     private val commandOrderPort: CommandOrderPort
 ) {
-    fun execute(itemId: UUID, orderItemRequest: OrderItemRequest) {
-        val student = securityService.queryCurrentUser().let { queryStudentPort.queryStudentByUser(it) ?: throw StudentNotFoundException() }
-        val item = queryItemPort.queryItemById(itemId) ?: throw ItemNotFoundException()
+    fun execute(orderItemRequest: OrderItemRequest) {
+        val student = securityService.queryCurrentUser().let { studentPort.queryStudentByUser(it) ?: throw StudentNotFoundException() }
 
-        val price = orderItemRequest.count * item.price
-        val sum = student.currentPoint - price
+        queryItemPort.queryAllItem().map {
+            val sum = (orderItemRequest.count * it.price).let { price ->
+                student.currentPoint - price
+            }
 
-        if (sum < 0)
-            throw LackPointException()
+            if (sum < 0)
+                throw LackPointException()
 
-        student.copy(currentPoint = sum)
+            studentPort.save(student.copy(currentPoint = sum))
 
-        val order = Order(
-            id = UUID.randomUUID(),
-            count = orderItemRequest.count,
-            price = sum,
-            orderStatus = OrderStatus.IS_ORDERED,
-            itemId = itemId,
-            studentId = student.id
-        )
+            val order = Order(
+                id = UUID.randomUUID(),
+                count = orderItemRequest.count,
+                price = sum,
+                orderStatus = OrderStatus.IS_ORDERED,
+                itemId = it.id,
+                studentId = student.id
+            )
 
-        commandOrderPort.save(order)
+            commandOrderPort.save(order)
+        }
     }
 }
