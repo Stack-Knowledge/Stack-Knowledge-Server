7f6a30408fb0bc21f0020b3e1f9159533c9fa272
JuuuuHong <juhang720@gmail.com>
2023-08-28 16:26:20 +0900

    refactor :: 아이템 지급 서비스

--- src/main/kotlin/com/stack/knowledege/domain/order/application/usecase/ProvideItemUseCase.kt
+++ src/main/kotlin/com/stack/knowledege/domain/order/application/usecase/ProvideItemUseCase.kt
@@ -1,28 +1,37 @@
 package com.stack.knowledege.domain.order.application.usecase
 
 import com.stack.knowledege.common.annotation.usecase.UseCase
+import com.stack.knowledege.domain.item.application.spi.QueryItemPort
+import com.stack.knowledege.domain.item.exception.ItemNotFoundException
 import com.stack.knowledege.domain.order.application.spi.OrderPort
 import com.stack.knowledege.domain.order.domain.constant.OrderStatus
 import com.stack.knowledege.domain.order.exception.AlreadyCompletedOrderException
 import com.stack.knowledege.domain.order.exception.LackOrderException
 import com.stack.knowledege.domain.order.exception.OrderNotFoundException
 import com.stack.knowledege.domain.order.presentation.data.request.UpdateOrderStatusRequest
-import java.util.UUID
 
 @UseCase
 class ProvideItemUseCase(
-    private val orderPort: OrderPort
+    private val orderPort: OrderPort,
+    private val queryItemPort: QueryItemPort
 ) {
-    fun execute(orderId: UUID, updateOrderStatusRequest: UpdateOrderStatusRequest) {
-        val order = orderPort.queryOrderById(orderId) ?: throw OrderNotFoundException()
+    fun execute(updateOrderStatusRequest: List<UpdateOrderStatusRequest>) {
+        val orders = orderPort.queryAllIsOrderedItem(OrderStatus.IS_ORDERED)
 
-        if (order.orderStatus == OrderStatus.COMPLETED)
-            AlreadyCompletedOrderException()
+        updateOrderStatusRequest.map {
+            val order = orderPort.queryOrderById(it.orderId) ?: throw OrderNotFoundException()
+            val item = queryItemPort.queryItemById(order.itemId) ?: throw ItemNotFoundException()
 
-        if (order.count - updateOrderStatusRequest.count < 0)
-            LackOrderException()
-        else if (order.count - updateOrderStatusRequest.count >= 0) {
-            orderPort.save(order.copy(count = order.count - updateOrderStatusRequest.count, orderStatus = OrderStatus.COMPLETED))
+            if (order.orderStatus != OrderStatus.IS_ORDERED)
+                AlreadyCompletedOrderException()
+
+            val saveOrder = when {
+                order.count > it.count -> order.copy(count = it.count - order.count, price = order.price - item.price * order.count)
+                order.count == it.count -> order.copy(count = 0, price = 0, orderStatus = OrderStatus.COMPLETED)
+                else -> throw LackOrderException()
+            }
+
+            orderPort.save(saveOrder)
         }
     }
 }
--- src/main/kotlin/com/stack/knowledege/domain/order/presentation/data/request/UpdateOrderStatusRequest.kt
+++ src/main/kotlin/com/stack/knowledege/domain/order/presentation/data/request/UpdateOrderStatusRequest.kt
@@ -1,5 +1,8 @@
 package com.stack.knowledege.domain.order.presentation.data.request
 
+import java.util.*
+
 data class UpdateOrderStatusRequest(
+    val orderId: UUID,
     val count: Int
 )
