d3a52afcc41ca70a8532559c4592e7857ae7231f
JuuuuHong <juhang720@gmail.com>
2023-09-01 08:01:57 +0900

    delete :: cache test

--- src/test/kotlin/com/stack/knowledege/global/annotation/StackKnowledgeTest.kt
+++
@@ -1,9 +0,0 @@
-package com.stack.knowledege.global.annotation
-
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.junit.jupiter.MockitoExtension
-//
-//@Target(AnnotationTarget.CLASS)
-//@Retention(AnnotationRetention.RUNTIME)
-//@ExtendWith(MockitoExtension::class)
-//annotation class StackKnowledgeTest
