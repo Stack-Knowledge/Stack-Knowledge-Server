a5fdcae8294e386ed2ac06b9b9fde1a5499c5c7d
JuuuuHong <juhang720@gmail.com>
2023-09-10 22:41:50 +0900

    refactor :: 페이징 처리

--- src/main/kotlin/com/stack/knowledge/domain/solve/application/spi/QuerySolvePort.kt
+++ src/main/kotlin/com/stack/knowledge/domain/solve/application/spi/QuerySolvePort.kt
@@ -3,10 +3,11 @@ package com.stack.knowledge.domain.solve.application.spi
 import com.stack.knowledge.domain.mission.domain.Mission
 import com.stack.knowledge.domain.solve.domain.Solve
 import com.stack.knowledge.domain.solve.domain.constant.SolveStatus
+import org.springframework.data.domain.Pageable
 import java.util.UUID
 
 interface QuerySolvePort {
-    fun queryAllSolveBySolveStatus(solveStatus: SolveStatus): List<Solve>
+    fun queryAllSolveBySolveStatus(solveStatus: SolveStatus, pageable: Pageable): List<Solve>
     fun querySolveById(solveId: UUID): Solve?
     fun querySolveByMission(mission: Mission): Solve?
     fun querySolveByStudentId(studentId: UUID): List<Solve>
--- src/main/kotlin/com/stack/knowledge/domain/solve/persistence/repository/SolveJpaRepository.kt
+++ src/main/kotlin/com/stack/knowledge/domain/solve/persistence/repository/SolveJpaRepository.kt
@@ -3,11 +3,12 @@ package com.stack.knowledge.domain.solve.persistence.repository
 import com.stack.knowledge.domain.mission.domain.Mission
 import com.stack.knowledge.domain.solve.domain.constant.SolveStatus
 import com.stack.knowledge.domain.solve.persistence.entity.SolveJpaEntity
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
 import java.util.UUID
 
 interface SolveJpaRepository : CrudRepository<SolveJpaEntity, UUID> {
     fun findByMission(mission: Mission): SolveJpaEntity?
-    fun findAllBySolveStatus(solveStatus: SolveStatus): List<SolveJpaEntity>
+    fun findAllBySolveStatus(solveStatus: SolveStatus, pageable: Pageable): List<SolveJpaEntity>
     fun findAllByStudentId(studentId: UUID): List<SolveJpaEntity>
 }
