c447167461530c8a7e2bd64dcef2c657765b25c0
JuuuuHong <juhang720@gmail.com>
2023-09-11 21:16:43 +0900

    add :: mission 최대 글자 길이

--- src/main/kotlin/com/stack/knowledge/domain/mission/presentation/data/request/CreateMissionRequest.kt
+++ src/main/kotlin/com/stack/knowledge/domain/mission/presentation/data/request/CreateMissionRequest.kt
@@ -1,11 +1,14 @@
 package com.stack.knowledge.domain.mission.presentation.data.request
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
 
 data class CreateMissionRequest(
     @field:NotBlank
+    @field:Size(max = 50)
     val title: String,
     @field:NotBlank
+    @field:Size(max = 500)
     val content: String,
     val timeLimit: Int
 )
