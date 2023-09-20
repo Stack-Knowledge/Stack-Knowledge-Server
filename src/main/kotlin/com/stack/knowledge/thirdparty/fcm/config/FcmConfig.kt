eefd45242dd72270696bfe5346ea7c93d33d8f98
JuuuuHong <juhang720@gmail.com>
2023-09-21 00:20:35 +0900

    add :: fcm config

--- src/main/kotlin/com/stack/knowledge/thirdparty/fcm/config/FcmConfig.kt
+++ src/main/kotlin/com/stack/knowledge/thirdparty/fcm/config/FcmConfig.kt
@@ -1,7 +1,11 @@
 package com.stack.knowledge.thirdparty.fcm.config
 
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import com.stack.knowledge.thirdparty.fcm.property.FcmProperties
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -14,10 +18,19 @@ class FcmConfig(
     @PostConstruct
     private fun initialize() {
         runCatching {
-            FirebaseA
-            URL(fcmProperties.fileUrl).openStream().use {
-                Files.copy(it, Paths.get(Path))
+            val options: FirebaseOptions
+            options.toBuilder()
+                .setCredentials(
+                    GoogleCredentials
+                        .fromStream(ClassPathResource(fcmProperties.fileUrl).inputStream)
+                        .createScoped(listOf("fireBase Scope"))
+                ).build()
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options)
             }
+        }.onFailure {
+            it.printStackTrace()
         }
     }
 }
