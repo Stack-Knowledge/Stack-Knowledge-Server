75fb8ff86328e19c2cb6e07154e38b3081ba84a7
JuuuuHong <juhang720@gmail.com>
2023-09-24 23:51:14 +0900

    update :: jwt request filter bean 등록 순선

--- src/main/kotlin/com/stack/knowledge/global/filter/JwtRequestFilter.kt
+++ src/main/kotlin/com/stack/knowledge/global/filter/JwtRequestFilter.kt
@@ -1,12 +1,15 @@
 package com.stack.knowledge.global.filter
 
 import com.stack.knowledge.global.security.spi.JwtParserPort
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+@Order(Ordered.HIGHEST_PRECEDENCE)
 class JwtRequestFilter(
     private val jwtParserPort: JwtParserPort
 ) : OncePerRequestFilter() {
