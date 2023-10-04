7b36b84f04fbb452f163fde7ae16d82b7fb96511
JuuuuHong <juhang720@gmail.com>
2023-10-04 10:29:38 +0900

    add :: valid exception

--- src/main/kotlin/com/stack/knowledge/global/error/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/stack/knowledge/global/error/handler/GlobalExceptionHandler.kt
@@ -4,6 +4,7 @@ import com.stack.knowledge.global.error.ErrorResponse
 import com.stack.knowledge.global.error.exception.StackKnowledgeException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(StackKnowledgeException::class)
     fun stackKnowledgeExceptionHandler(e: StackKnowledgeException): ResponseEntity<ErrorResponse> =
-        ResponseEntity(
-            ErrorResponse(message = e.errorCode.message, status = e.errorCode.status),
-            HttpStatus.valueOf(e.errorCode.status)
-        )
+        ResponseEntity.status(HttpStatus.valueOf(e.errorCode.status)).body(ErrorResponse(message = e.errorCode.message, status = e.errorCode.status))
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> =
+        ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(ErrorResponse(e.bindingResult.fieldError?.defaultMessage.toString(), HttpStatus.BAD_REQUEST.value()))
 }
