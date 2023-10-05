09218fecaa7606bf89695c8bfcab3601f5d58464
JuuuuHong <juhang720@gmail.com>
2023-10-06 00:46:00 +0900

    update :: scoring page 자신이 등록한 문제만 볼 수 있게

--- src/main/kotlin/com/stack/knowledge/domain/solve/persistence/SolvePersistenceAdapter.kt
+++ src/main/kotlin/com/stack/knowledge/domain/solve/persistence/SolvePersistenceAdapter.kt
@@ -1,12 +1,12 @@
 package com.stack.knowledge.domain.solve.persistence
 
 import com.stack.knowledge.domain.mission.domain.Mission
+import com.stack.knowledge.domain.mission.persistence.mapper.MissionMapper
 import com.stack.knowledge.domain.solve.application.spi.SolvePort
 import com.stack.knowledge.domain.solve.domain.Solve
 import com.stack.knowledge.domain.solve.domain.constant.SolveStatus
 import com.stack.knowledge.domain.solve.persistence.mapper.SolveMapper
 import com.stack.knowledge.domain.solve.persistence.repository.SolveJpaRepository
-import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import java.util.*
@@ -14,19 +14,20 @@ import java.util.*
 @Component
 class SolvePersistenceAdapter(
     private val solveJpaRepository: SolveJpaRepository,
-    private val solveMapper: SolveMapper
+    private val solveMapper: SolveMapper,
+    private val missionMapper: MissionMapper
 ) : SolvePort {
     override fun save(solve: Solve): Solve? =
        solveMapper.toDomain(solveJpaRepository.save(solveMapper.toEntity(solve)))
 
-    override fun queryAllSolveBySolveStatus(solveStatus: SolveStatus, pageable: Pageable): List<Solve> =
-        solveJpaRepository.findAllBySolveStatus(solveStatus, pageable).map { solveMapper.toDomain(it)!! }
+    override fun queryAllSolveBySolveStatus(solveStatus: SolveStatus, mission: Mission): List<Solve> =
+        solveJpaRepository.findAllBySolveStatusAndMission(solveStatus, missionMapper.toEntity(mission)).map { solveMapper.toDomain(it)!! }
 
     override fun querySolveById(solveId: UUID): Solve? =
         solveMapper.toDomain(solveJpaRepository.findByIdOrNull(solveId))
 
     override fun querySolveByMission(mission: Mission): Solve? =
-        solveMapper.toDomain(solveJpaRepository.findByMission(mission))
+        solveMapper.toDomain(solveJpaRepository.findByMission(missionMapper.toEntity(mission)))
 
     override fun querySolveByStudentId(studentId: UUID): List<Solve> =
         solveJpaRepository.findAllByStudentId(studentId).map { solveMapper.toDomain(it)!! }
