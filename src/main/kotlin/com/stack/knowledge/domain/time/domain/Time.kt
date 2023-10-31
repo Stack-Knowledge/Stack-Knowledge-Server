059317d018e32fea0a3070c983ff4281b0833c67
JuuuuHong <juhang720@gmail.com>
2023-11-01 08:47:07 +0900

    add :: time mapper

--- src/main/kotlin/com/stack/knowledge/domain/time/domain/Time.kt
+++ src/main/kotlin/com/stack/knowledge/domain/time/domain/Time.kt
@@ -1,11 +1,10 @@
 package com.stack.knowledge.domain.time.domain
 
-import com.stack.knowledge.domain.mission.domain.Mission
 import java.time.LocalDateTime
-import java.util.UUID
+import java.util.*
 
 data class Time(
     val id: UUID,
-    val mission: Mission,
+    val mission: UUID,
     val createdAt: LocalDateTime
 )
