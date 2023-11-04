0b5dc2976e04f6639127d78409f10c29a89aa7da
JuuuuHong <juhang720@gmail.com>
2023-11-04 20:09:49 +0900

    add :: find all mission query n+1 문제 해결

--- src/main/kotlin/com/stack/knowledge/domain/mission/persistence/MissionPersistenceAdapter.kt
+++ src/main/kotlin/com/stack/knowledge/domain/mission/persistence/MissionPersistenceAdapter.kt
@@ -5,7 +5,6 @@ import com.stack.knowledge.domain.mission.domain.Mission
 import com.stack.knowledge.domain.mission.domain.constant.MissionStatus
 import com.stack.knowledge.domain.mission.persistence.mapper.MissionMapper
 import com.stack.knowledge.domain.mission.persistence.repository.MissionJpaRepository
-import com.stack.knowledge.domain.user.persistence.mapper.UserMapper
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 import java.util.*
@@ -13,8 +12,7 @@ import java.util.*
 @Component
 class MissionPersistenceAdapter(
     private val missionJpaRepository: MissionJpaRepository,
-    private val missionMapper: MissionMapper,
-    private val userMapper: UserMapper
+    private val missionMapper: MissionMapper
 ) : MissionPort {
     override fun save(mission: Mission) {
         missionMapper.toDomain(missionJpaRepository.save(missionMapper.toEntity(mission)))
@@ -23,9 +21,9 @@ class MissionPersistenceAdapter(
     override fun queryMissionById(missionId: UUID): Mission? =
         missionMapper.toDomain(missionJpaRepository.findByIdOrNull(missionId))
 
-    override fun queryAllMissionByUserId(userId: UUID): List<Mission> =
+    override fun queryAllMissionsByUserId(userId: UUID): List<Mission> =
         missionJpaRepository.findAllByUserId(userId).map { missionMapper.toDomain(it)!! }
 
     override fun queryAllMissionByMissionStatusOrderByCreatedAtDesc(missionStatus: MissionStatus): List<Mission> =
-        missionJpaRepository.findByMissionStatusOrderByCreatedAtDesc(missionStatus).map { missionMapper.toDomain(it)!! }
+        missionJpaRepository.findAllByMissionStatusOrderByCreatedAtDesc(missionStatus).map { missionMapper.toDomain(it)!! }
 }
