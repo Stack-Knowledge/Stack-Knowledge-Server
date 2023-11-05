1934ce7d27643dd03c0976ac12e976973a6de0f4
JuuuuHong <juhang720@gmail.com>
2023-11-05 23:31:05 +0900

    update :: solve usecase transactional

--- src/main/kotlin/com/stack/knowledge/domain/solve/application/usecase/SolveMissionUseCase.kt
+++ src/main/kotlin/com/stack/knowledge/domain/solve/application/usecase/SolveMissionUseCase.kt
@@ -1,8 +1,8 @@
 package com.stack.knowledge.domain.solve.application.usecase
 
-import com.stack.knowledge.common.annotation.usecase.UseCase
 import com.stack.knowledge.common.service.SecurityService
 import com.stack.knowledge.domain.mission.application.spi.MissionPort
+import com.stack.knowledge.domain.mission.domain.Mission
 import com.stack.knowledge.domain.mission.domain.constant.MissionStatus
 import com.stack.knowledge.domain.mission.exception.MissionNotFoundException
 import com.stack.knowledge.domain.mission.exception.MissionNotOpenedException
@@ -17,11 +17,14 @@ import com.stack.knowledge.domain.solve.presentation.data.request.SolveMissionRe
 import com.stack.knowledge.domain.time.application.spi.QueryTimePort
 import com.stack.knowledge.domain.time.exception.TimeLimitExceededException
 import com.stack.knowledge.domain.time.exception.TimeNotFoundException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.Duration
 import java.time.LocalDateTime
 import java.util.*
 
-@UseCase
+//@UseCase
+@Service
 class SolveMissionUseCase(
     private val missionPort: MissionPort,
     private val solvePort: SolvePort,
@@ -29,6 +32,7 @@ class SolveMissionUseCase(
     private val pointPort: PointPort,
     private val queryTimePort: QueryTimePort
 ) {
+    @Transactional(noRollbackFor = [Exception::class])
     fun execute(id: UUID, solveMissionRequest: SolveMissionRequest) {
         val mission = missionPort.queryMissionById(id) ?: throw MissionNotFoundException()
         val studentId = securityService.queryCurrentUserId()
@@ -42,26 +46,36 @@ class SolveMissionUseCase(
         val time = queryTimePort.queryTimeByMission(mission) ?: throw TimeNotFoundException()
         val timeElapsed = (Duration.between(time.createdAt, LocalDateTime.now())).toSeconds()
 
-        if (timeElapsed > mission.timeLimit + 1)
+        val (solve, isExceeded) = createSolve(timeElapsed, mission, solveMissionRequest.solvation, studentId)
+
+        if (isExceeded)
             throw TimeLimitExceededException()
-
-        val solve = Solve(
-            id = UUID.randomUUID(),
-            solvation = solveMissionRequest.solvation,
-            solveStatus = SolveStatus.SCORING,
-            student = studentId,
-            mission = mission.id
-        )
-        val saveSolve = solvePort.save(solve) ?: throw SolveNotFoundException()
-
 
         val topPoint = (pointPort.queryTopByMissionIdOrderByMissionPointDesc(mission.id)?.missionPoint?.times(0.97))?.toInt()
 
         val point = Point(
             missionPoint = topPoint ?: 1000,
             mission = mission.id,
-            solve = saveSolve.id
+            solve = solve.id
         )
         pointPort.save(point)
+    }
+
+    private fun createSolve(timeElapsed: Long, mission: Mission, solvation: String, studentId: UUID): Pair<Solve, Boolean> {
+        val (solveStatus, isExceeded) = when {
+            timeElapsed > mission.timeLimit + 1 -> Pair(SolveStatus.WRONG_ANSWER, true)
+            else -> Pair(SolveStatus.SCORING, false)
+        }
+
+        val solve = Solve(
+            id = UUID.randomUUID(),
+            solvation = solvation,
+            solveStatus = solveStatus,
+            student = studentId,
+            mission = mission.id
+        )
+        val saveSolve = solvePort.save(solve) ?: throw SolveNotFoundException()
+
+        return Pair(saveSolve, isExceeded)
     }
 }
