1a3b3944721e7b6354efcb2f71585fc28d02d00c
JuuuuHong <juhang720@gmail.com>
2024-04-22 18:04:24 +0900

    update :: gauth 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
 	// GAuth
-	implementation("com.github.GSM-MSG:GAuth-SDK-Java:v2.0.0")
+	implementation("com.github.GSM-MSG:GAuth-SDK-Java:v3.0.0")
 
 	// aws
 	implementation("org.springframework.cloud:spring-cloud-starter-aws:2.0.1.RELEASE")
